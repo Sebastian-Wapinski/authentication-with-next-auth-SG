d8b64a5a43c73603988530209e38f0ef6d4011be
Sebastian <sebastian.pawel.wapinski@gmail.com>
2024-01-27 12:54:44 +0100

    form errors added

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,15 @@
 "use server";
 
+import { auth } from "@/auth";
 import { z } from "zod";
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
 
 const createTopicSchema = z.object({
   name: z
@@ -12,15 +21,33 @@ const createTopicSchema = z.object({
   description: z.string().min(10),
 });
 
-export async function createTopic(formData: FormData) {
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
   const result = createTopicSchema.safeParse({
     name: formData.get("name"),
     description: formData.get("description"),
   });
 
   if (!result.success) {
-    console.log(result.error.flatten().fieldErrors);
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
   }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this"],
+      },
+    };
+  }
+
+  return {
+    errors: {},
+  };
 
   // TODO: revalidate the homePage
 }
